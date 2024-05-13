c9c18e3fc6f5dae406dd1512535ecb79d484e08b
Daniil Zabludovsky <85434359+defenestration1111111@users.noreply.github.com>
2024-05-14 01:48:07 +0300

    refactor: add custom error

--- src/class_reader.rs
+++ src/class_reader.rs
@@ -1,3 +1,12 @@
+type Result<T> = std::result::Result<T, ReadError>;
+
+#[derive(Debug, thiserror::Error, PartialEq)]
+pub enum ReadError {
+    #[error("End of file encountered unexpectedly")]
+    #[non_exhaustive]
+    UnexpectedEOF,
+}
+
 struct ClassReader<'a> {
     buf: &'a [u8],
     pos: usize,
@@ -8,26 +17,26 @@ impl<'a> ClassReader<'a> {
         ClassReader { buf: data, pos: 0}
     }
 
-    fn read_bytes(&mut self, size: usize) -> Result<&'a [u8], &'static str> {
+    fn read_bytes(&mut self, size: usize) -> Result<&'a [u8]> {
         if self.pos + size > self.buf.len() {
-            Err("Error reading bytes")
+            Err(ReadError::UnexpectedEOF)
         } else {
             let bytes = &self.buf[self.pos..self.pos + size];
             Ok(bytes)
         }
     }
 
-    fn read_u8(&mut self) -> Result<u8, &'static str> {
+    fn read_u8(&mut self) -> Result<u8> {
         let slice = self.read_bytes(std::mem::size_of::<u8>())?;
         Ok(u8::from_be_bytes([slice[0]]))
     }
 
-    fn read_u16(&mut self) -> Result<u16, &'static str> {
+    fn read_u16(&mut self) -> Result<u16> {
         let slice = self.read_bytes(std::mem::size_of::<u16>())?;
         Ok(u16::from_be_bytes([slice[0], slice[1]]))
     }
 
-    fn read_u32(&mut self) -> Result<u32, &'static str> {
+    fn read_u32(&mut self) -> Result<u32> {
         let slice = self.read_bytes(std::mem::size_of::<u32>())?;
         Ok(u32::from_be_bytes([
             slice[0],
@@ -40,14 +49,24 @@ impl<'a> ClassReader<'a> {
 
 #[cfg(test)]
 mod tests {
+    use crate::class_reader::ReadError;
+
     use super::ClassReader;
 
     #[test]
-    fn test_magic() {
+    fn test_magic_success() {
         let data = [0xCA, 0xFE, 0xBA, 0xBE];
         let mut reader = ClassReader::new(&data);
         let magic = reader.read_u32().unwrap();
         
         assert_eq!(magic, 0xCAFEBABE);
+    }
+
+    #[test]
+    fn test_error_eof() {
+        let data = [0xCA];
+        let mut reader = ClassReader::new(&data);
+        
+        assert_eq!(reader.read_u32(), Err(ReadError::UnexpectedEOF));
     }
 }
