c23b475568a13e6963f95c0c507aa1c31c982a8f
Daniil Zabludovsky <85434359+defenestration1111111@users.noreply.github.com>
2025-03-13 03:22:56 +0300

    feat: implement bitwise & logical operations & tests

--- interpreter/src/instruction_executor.rs
+++ interpreter/src/instruction_executor.rs
@@ -1,6 +1,6 @@
 use rsjvm_class_reader::instruction::Instruction::{self, *};
 
-use crate::frame::{Frame, Value};
+use crate::frame::{binary_op, unary_op, Frame, Value};
 
 struct InstructionExecutor;
 
@@ -25,6 +25,7 @@ impl InstructionExecutor {
             Fconst_2 => {
                 frame.push_operand(Value::Float(2.0));
             },  
+            Iand => binary_op::<i32, _>(|a, b| a & b, frame),
             Iconst_m1 => {
                 frame.push_operand(Value::Int(-1));
             },  
@@ -46,13 +47,65 @@ impl InstructionExecutor {
             Iconst_5 => {
                 frame.push_operand(Value::Int(5));
             },  
+            Ior => binary_op::<i32, _>(|a, b| a | b, frame),
+            Ishl => binary_op::<i32, _>(|a, b| a << (b & 0x1F), frame),
+            Ishr => binary_op::<i32, _>(|a, b| a >> (b & 0x1F), frame),
+            Iushr => binary_op::<i32, _>(|a, b| {
+            Ixor => binary_op::<i32, _>(|a, b| a ^ b, frame),
+            Land => binary_op::<i64,_>(|a, b| a & b, frame),
             Lconst_0 => {
                 frame.push_operand(Value::Long(0));
             },  
             Lconst_1 => {
                 frame.push_operand(Value::Long(1));
             },  
+            Lor => binary_op::<i64, _>(|a, b| a | b, frame),
+            Lshl => binary_op::<i64, _>(|a, b| a << (b & 0x3F), frame),
+            Lshr => binary_op::<i64, _>(|a, b| a >> (b & 0x3F), frame),
+            Lushr => binary_op::<i64, _>(|a, b| {
+            Lxor => binary_op::<i64, _>(|a, b| a ^ b, frame),
             _ => unimplemented!(),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::frame::Frame;
+    use crate::frame::Value;
+    use crate::instruction_executor::InstructionExecutor;
+
+    fn setup_frame() -> Frame {
+        Frame::new(0, 2)
+    }
+
+    macro_rules! param_test {
+        ($($name:ident: ($instr:expr, $a:expr, $b:expr) => $expected:expr),* $(,)?) => {
+            $(
+                #[test]
+                fn $name() {
+                    let mut frame = setup_frame();
+                    frame.push_operand(Value::Int($a));
+                    frame.push_operand(Value::Int($b));
+                    InstructionExecutor::execute($instr, &mut frame);
+                    let result = frame.pop_operand();
+
+                    match result {
+                        Value::Int(value) => assert_eq!(value, $expected, "Failed for instruction {:?} with inputs ({}, {})", $instr, $a, $b),
+                        _ => panic!("Expected Value::Int but got {:?}", result),
+                    }
+                }
+            )*
+        };
+    }
+
+    param_test! {
+        test_ishr_16_2: (super::Ishr, 16, 2) => 4, // 16 >> 2 = 4
+
+        test_iushr_16_2: (super::Iushr, 16, 2) => 4, // 16 >>> 2 = 4
+        test_iushr_neg16_2: (super::Iushr, -16, 2) => 1073741820, // -16 >>> 2 = 1073741820
+        test_iushr_5_0: (super::Iushr, 5, 0) => 5, // 5 >>> 0 = 5
+        test_iushr_neg1_31: (super::Iushr, -1, 31) => 1, // -1 >>> 31 = 1
+    }
+
 }
